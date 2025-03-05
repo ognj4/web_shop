5a8c792fbb156f9687e28075426b2298ebd81940
ognj4 <stepanovicognjenos@gmail.com>
2025-03-05 15:01:17 +0100

    * Old routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,40 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('/about','about');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::get('/',[HomepageController::class,'index']);
+Route::get('/shop',[ShopController::class,'index']);
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+Route::get('/admin/all-contacts', [ContactController::class,'getAllContacts']);
+
+Route::get('/admin/all-products', [ProductsController::class,'index'])
+    ->name('sviProizvodi');
+
+Route::get('/admin/delete-product/{product}', [ProductsController::class,'delete'])
+    ->name('obrisiProizvod');
+Route::get('/admin/delete-contact/{contact}', [ContactController::class, "delete"])
+    ->name('obrisiKontakt');
+
+Route::view('/admin/add-product', 'addProduct');
+Route::post('/admin/save-product',[ProductsController::class, 'saveProduct'])
+    ->name('snimanjeOglasa');
+
+Route::post('/send-contact', [ContactController::class,'sendContact']);
+
+Route::get('admin/product/edit/{product}',[ProductsController::class,'singleProduct'])
+    ->name('product.single');
+
+Route::post('admin/product/save/{product}',[ProductsController::class,'edit'])
+    ->name('product.save');
+
 
 require __DIR__.'/auth.php';
