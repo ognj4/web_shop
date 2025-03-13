11dbb7776e7dff22fc38b6b0d248f45e9a53281b
ognj4 <stepanovicognjenos@gmail.com>
2025-03-13 18:46:52 +0100

    * Command for API #D20

---
+++ app/Console/Commands/GetDailyCurrencyValue.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\ExchangeRates;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class GetDailyCurrencyValue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'currency:get-values';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get currency values for USD and EUR';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $currencies = ['usd','eur','rub'];
+
+        foreach ($currencies as $currency)
+        {
+            $response = Http::get("https://kurs.resenje.org/api/v1/currencies/$currency/rates/today");
+            ExchangeRates::create([
+                'currency' => $currency,
+                'value' => $response->json()['exchange_middle']
+            ]);
+
+        }
+
+    }
+}
