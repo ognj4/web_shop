5dc4c21ab591e927605edcf56e5e7de49c3c73ea
ognj4 <stepanovicognjenos@gmail.com>
2025-03-13 18:47:31 +0100

    * Migration and model for storing currency and value

---
+++ app/Models/ExchangeRates.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExchangeRates extends Model
+{
+    protected $table = 'exchange_rates';
+    protected $fillable = [
+        'currency', 'value'
+    ];
+}
