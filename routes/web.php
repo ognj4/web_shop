8dabb775ac7740b0ce8295d3644d3278a92620bc
ognj4 <stepanovicognjenos@gmail.com>
2025-03-24 17:15:53 +0100

    * Prefix contacts routes

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('admin')->group
 
     Route::view('/add-product', 'addProduct');
 
-    Route::controller(ContactController::class)->group(function () {
-        Route::get('/contact/all', 'getAllContacts');
-        Route::post('/contact/send','sendContact')->name('sendContact');
-        Route::get('/contact/delete/{contact}','delete')->name('obrisiKontakt');
+    Route::controller(ContactController::class)->prefix('/contact')->group(function () {
+        Route::get('/all', 'getAllContacts');
+        Route::post('/send','sendContact')->name('sendContact');
+        Route::get('/delete/{contact}','delete')->name('obrisiKontakt');
     });
 
     Route::controller(ProductsController::class)->group(function (){
