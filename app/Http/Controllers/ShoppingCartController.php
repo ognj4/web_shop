dca5e5dbb6a068110142e952f2ce4f35df8d9696
ognj4 <stepanovicognjenos@gmail.com>
2025-03-26 11:21:54 +0100

    * Check for amount

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartAddRequest;
+use App\Models\ProductsModel;
 use Illuminate\Support\Facades\Session;
 
 class ShoppingCartController extends Controller
@@ -15,6 +16,12 @@ class ShoppingCartController extends Controller
     }
     public function addToCart(CartAddRequest $request)
     {
+        $product = ProductsModel::where(['id'=> $request->id])->first();
+
+        if ($product->amount < $request->amount){
+            return redirect()->back();
+        }
+
         Session::push('product',[
             'product_id' => $request->id,
             'amount' => $request->amount,
